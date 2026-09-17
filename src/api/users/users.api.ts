bcf7137f0f56e463c1122a206713b0112b9c058c
aoqilov <aoqilov88@gmail.com>
2026-09-17 16:34:43 +0500

    test

--- src/api/users/users.api.ts
+++ src/api/users/users.api.ts
@@ -28,6 +28,6 @@ export const usersApi = {
   /** Telegram initdata yoki qubnix_session cookie orqali joriy foydalanuvchini oladi. */
   me: () =>
     axiosInstance
-      .get<ApiEnvelope<{ user: RawUser }>>("/api/v1/users/me")
+      .get<ApiEnvelope<{ user: RawUser }>>("/api/v1/users/me2")
       .then((r) => toSessionUser(r.data.data.user)),
 };
